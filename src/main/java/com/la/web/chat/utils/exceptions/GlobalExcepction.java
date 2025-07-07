7e0aa3a6d78fc31d83d256d0da7774ea93455c52
daniel-la <03daniel030399@gmail.com>
2025-07-07 14:47:13 -0600

    resolve erro in global exception

--- src/main/java/com/la/web/chat/utils/exceptions/GlobalExcepction.java
+++ src/main/java/com/la/web/chat/utils/exceptions/GlobalExcepction.java
@@ -1,5 +1,7 @@
 package com.la.web.chat.utils.exceptions;
 
+import java.util.Map;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
