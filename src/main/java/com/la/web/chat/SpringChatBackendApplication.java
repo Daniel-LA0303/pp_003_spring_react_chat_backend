56c916cfb5a7f2d25a243161fb36f77db86f5b53
daniel-la <03daniel030399@gmail.com>
2025-07-07 13:26:05 -0600

    init project in blank

---
+++ src/main/java/com/la/web/chat/SpringChatBackendApplication.java
@@ -0,0 +1,13 @@
+package com.la.web.chat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringChatBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringChatBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/la/web/chat/SpringChatBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.la.web.chat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringChatBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
